d3936da970b1c23cfcf7ebf0bd14b22f53763e29
Brian Doolittle <briand4@illinois.edu>
2020-06-21 16:45:59 -0500

    adding notebook and matplotlib

--- test/module.py
+++ test/module.py
@@ -1,7 +1,7 @@
 import unittest
+import numpy as np
 
 from module import example
-
 
 class module_test_cases(unittest.TestCase):
     def test_hello(self):
@@ -11,3 +11,10 @@ class module_test_cases(unittest.TestCase):
     def test_add_two(self):
         self.assertEqual(4, example.add_two(2))
         self.assertEqual(7, example.add_two(5))
+
+    def test_parabola(self):
+        self.assertTrue(
+            [9,4,1,0,1,4] == list( map(
+                example.parabola,  np.arange(-3,3)
+            )) 
+        )
