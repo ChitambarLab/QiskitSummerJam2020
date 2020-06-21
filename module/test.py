782d5d7db31d188565fb14771b8256179cc85dc2
Brian Doolittle <briand4@illinois.edu>
2020-06-21 12:05:14 -0500

    test framework

--- module/test.py
+++ module/test.py
@@ -1,5 +1,8 @@
 def hello():
     print("hello")
 
+def add_two(x):
+    return x + 2
+
 if __name__ == "__main__":
     hello()
--- test/module.py
+++ test/module.py
@@ -1,3 +1,13 @@
+import unittest
+
 from module import test
 
-test.hello()
+
+class module_test_cases(unittest.TestCase):
+    def test_hello(self):
+        test.hello()
+        pass
+    
+    def test_add_two(self):
+        self.assertEqual(4, test.add_two(2))
+        self.assertEqual(7, test.add_two(5))
