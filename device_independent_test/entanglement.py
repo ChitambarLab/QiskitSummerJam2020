a6650f02d8668ae3e3fe5d8c3b4d50fb32d9d956
unknown <xchen146@illinois.edu>
2020-06-25 16:25:45 -0500

    built circuits for chsh test
    preliminary version of the entanglement notebook

--- device_independent_test/entanglement.py
+++ device_independent_test/entanglement.py
@@ -1,1 +1,42 @@
-# Tests the presence of entanglement and Measurement Incompatibility
+from qiskit import QuantumCircuit
+
+# create two-qubit entangled circuit
+def create_entangled_circuit():
+  qc = QuantumCircuit(2)
+  qc.h(0)
+  qc.cx(0,1)
+  return qc
+
+# measure a given circuit in the ZW basis
+def measure_in_ZW(qc):
+  qc.s(1)
+  qc.h(1)
+  qc.t(1)
+  qc.h(1)
+  return qc.measure_all()
+
+# measure a given circuit in the ZV basis
+def measure_in_ZV(qc):
+  qc.s(1)
+  qc.h(1)
+  qc.tdg(1)
+  qc.h(1)
+  return qc.measure_all()
+
+# measure a given circuit in the XW basis
+def measure_in_XW(qc):
+  qc.h(0)
+  qc.s(1)
+  qc.h(1)
+  qc.t(1)
+  qc.h(1)
+  return qc.measure_all()
+
+# measure a given circuit in the XV basis
+def measure_in_XV(qc):
+  qc.h(0)
+  qc.s(1)
+  qc.h(1)
+  qc.tdg(1)
+  qc.h(1)
+  return qc.measure_all()
